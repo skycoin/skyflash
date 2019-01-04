eace0f24b832209047e0925ff8013915fcaeeb2d
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-01-04 00:47:56 -0500

    Python script switch to QtQuick/QML

--- skyflash.py
+++ skyflash.py
@@ -1,228 +1,107 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-# basic libs
-import sys
-import os
-import traceback
+from PyQt5.QtGui import QGuiApplication
+# from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import QObject, QUrl, pyqtSlot, pyqtSignal
+import os, sys, subprocess, webbrowser
 
-try:
-    input = raw_input
-except:
-    pass
+# environment vars
+skybianUrl = "http://localhost:8080/skybian.tar.xz"
+manualUrl = "http://github.com/simelo/skyflash"
 
-# QT5 imports
-try:
-    from PyQt5.QtGui import *
-    from PyQt5.QtWidgets import *
-    from PyQt5.QtCore import *
-except ImportError as error:
-    print('Import error: {}'.format(error))
-    input('\nError importing libraries\nPress [Return] to exit')
-    exit(1)
 
-# main app
-class ApplicationWindow(QMainWindow):
-
-    # initializer function
-    # Basically draw the interface and init the vars
+# main object definition
+class skyFlash(QObject):
     def __init__(self):
-        QMainWindow.__init__(self)
-        self.setAttribute(Qt.WA_DeleteOnClose)
-        self.setWindowTitle("Skyflash")
+        QObject.__init__(self)
+        bimg = ""
 
-        self.file_menu = QMenu('&File', self)
-        self.file_menu.addAction('&Quit', self.fileQuit, Qt.CTRL + Qt.Key_Q)
+    # registering Signals to emit to QT-QML
+    downloadUpdate = pyqtSignal(int, arguments=["percent"])
+    downloadFinished = pyqtSignal(str, arguments=["msg"])
+    setStatus = pyqtSignal(str, arguments=["msg"])
 
-        self.menuBar().addMenu(self.file_menu)
 
-        self.help_menu = QMenu('&Help', self)
-        self.menuBar().addSeparator()
-        self.menuBar().addMenu(self.help_menu)
+    @pyqtSlot()
+    def downloadSkybian(self):
+        # get default URL for downloads
+        Url = skybianUrl
 
-        self.help_menu.addAction('&About', self.about)
+        # DEBUG
+        print("Entrando a download...")
 
-        self.main_widget = QWidget(self)
+        # some validation
+        if Url is "" or "http" not in Url:
+            self.setStatus.emit("URL is empty or mangled, please check")
 
-        #### main layout
-        mainLayout = QVBoxLayout(self.main_widget)
+        # set status
+        self.setStatus.emit("Starting the download from the official site")
+        self.downloadFinished.emit("/tmp/Skybian-1.0.img")
 
-        #### image widget definition
-        image_widget = QVBoxLayout()
 
-        # adding widgets to the layout 
-        limage = QLabel("1. Skybian base image source")
-        image_widget.addWidget(limage)
+    @pyqtSlot()
+    def openManual(self):
+        if sys.platform == "win32":
+            try:
+                os.startfile(manualUrl)
+            except:
+                webbrowser.open(manualUrl)
 
-        # adding the horizontal layout for the buttons
-        image_buttons = QHBoxLayout()
+        elif sys.platform == "darwin":
+            subprocess.Popen(["open", manualUrl])
 
-        # push buttons % label
-        bdownload = QPushButton(text="Download Skybian", width=16)
-        blocal = QPushButton(text="Browse local",  width=16)
-        limagebuttons = QLabel("Select a local image or download one")
+        else:
+            try:
+                subprocess.Popen(["xdg-open", manualUrl])
+            except OSError:
+                print("Please open a browser on: " + manualUrl)
+
+
+    @pyqtSlot(str)
+    def localFile(self, file):
+        if file is "":
+            self.setStatus.emit("You selected nothing, plase try again")
+            return
         
-        # add elements to image button widgets
-        image_buttons.addWidget(bdownload)
-        image_buttons.addWidget(blocal)
-        image_buttons.addWidget(limagebuttons)
+        if file.startswith("file://"):
+            file = file.replace("file://", "")
+
+        print("Selected file is " + file)
+
+        # try to read a chunk of it
+        try:
+            self.bimg = open(file, 'rb')
+            dump = self.bimg.read(10)
+            self.bimg.close()
         
-        # add widget to image widget
-        image_widget.addLayout(image_buttons) 
+        except:
+            # TODO exact error
+            self.setStatus.emit("Selected file is not readable.")
+            return
 
-        # add it to main widget
-        mainLayout.addLayout(image_widget)
+        # detect the filename
+        # TODO OS dependent FS char
+        fpath = file.split("/")
+        filename = fpath[-1]
+        if len(filename) > 26:
+            filename = "..." + filename[-26:]
 
-        #### net widget definition
-        net_widget = QVBoxLayout()
+        self.downloadFinished.emit(filename)
+        
 
-        # network label
-        lnet = QLabel("2. Network settings")
-        net_widget.addWidget(lnet)
+app = QGuiApplication(sys.argv)
+# app = QApplication(sys.argv)
+skyflash = skyFlash()
+engine = QQmlApplicationEngine()
+engine.rootContext().setContextProperty("skf", skyflash)
+engine.load("skyflash.qml")
+engine.quit.connect(app.quit)
 
-        # check box 
-        self.cskyminer_default = QCheckBox("Use skyminers default (1 manager and 7 nodes)")
-        self.cskyminer_default.setChecked(True)
-        net_widget.addWidget(self.cskyminer_default)
 
-        # gateway & manager layout
-        image_gateway = QHBoxLayout()
-        image_manager = QHBoxLayout()
+if __name__ == "__main__":
+    try:
+        sys.exit(app.exec_())
+    except:
+        sys.exit("Ooops!")
 
-        # element def
-        lgateway = QLabel("Gateway:")
-        self.tgateway = QLineEdit("192.168.0.1", width=16)
-        ldns = QLabel("DNS:")
-        self.tdns = QLineEdit("1.0.0.1, 1.1.1.1", width=30)
-
-        # add them
-        image_gateway.addWidget(lgateway)
-        image_gateway.addWidget(self.tgateway)
-        image_gateway.addWidget(ldns)
-        image_gateway.addWidget(self.tdns)
-
-        # add layout to net
-        net_widget.addLayout(image_gateway)
-
-        # elements
-        lmanager = QLabel("Manager IP:")
-        self.tmanager = QLineEdit("192.168.0.1", width=16)
-        lnodescount = QLabel("Nodes to generate:")
-        self.tnodescount = QLineEdit("7", width=4)
-
-        # add them
-        image_manager.addWidget(lmanager)
-        image_manager.addWidget(self.tmanager)
-        image_manager.addWidget(lnodescount)
-        image_manager.addWidget(self.tnodescount)
-        image_manager.addStretch()
-
-        # add layout to net
-        net_widget.addLayout(image_manager)
-
-        # add it to main widget
-        mainLayout.addLayout(net_widget)
-
-        #### build widget definition
-        build_widget = QVBoxLayout()
-
-        # build label
-        lbuild = QLabel("3. Build images")
-        build_widget.addWidget(lbuild)
-
-        # layout for build button and progress bar
-        build_build_widget = QHBoxLayout()
-
-        # build button
-        bbuild = QPushButton(text="Build Images", width=16)
-        build_build_widget.addWidget(bbuild)
-
-        # progress bar
-        self.buildbar = QProgressBar()
-        self.buildbar.isTextVisible = True
-        self.buildbar.setMaximum(100)
-        self.buildbar.setMinimum(0)
-        self.buildbar.setValue(0)
-        build_build_widget.addWidget(self.buildbar)
-
-        # add build build layout to the build layout
-        build_widget.addLayout(build_build_widget)
-
-        # add it to main widget
-        mainLayout.addLayout(build_widget)
-
-        #### flash widget definition
-        flash_widget = QVBoxLayout()
-
-        lflash = QLabel("4. Flash the uSD cards, select the card & press the button")
-        flash_widget.addWidget(lflash)
-
-        # layout for combo + button
-        flash_flash_widget = QHBoxLayout()
-
-        # combo box, the updates will be timed on data change
-        # TODO auto update of the mmcblk devices
-        self.cbusd = QComboBox()
-        flash_flash_widget.addWidget(self.cbusd)
-
-        # flash button
-        self.bflash = QPushButton(text="Flash all uSD cards")
-        flash_flash_widget.addWidget(self.bflash)
-
-        # add layout to parent
-        flash_widget.addLayout(flash_flash_widget)
-
-        # flash this card progress bar
-        self.flashsdbar = QProgressBar()
-        self.flashsdbar.isTextVisible = True
-        self.flashsdbar.setMaximum(100)
-        self.flashsdbar.setMinimum(0)
-        self.flashsdbar.setValue(0)
-        flash_widget.addWidget(self.flashsdbar)
-
-        # flash ll cards progress bar
-        self.flashallbar = QProgressBar()
-        self.flashallbar.isTextVisible = True
-        self.flashallbar.setMaximum(100)
-        self.flashallbar.setMinimum(0)
-        self.flashallbar.setValue(0)
-        flash_widget.addWidget(self.flashallbar)
-
-        # add it to the main layout
-        mainLayout.addLayout(flash_widget)
-
-        # Status bar
-        self.status = self.statusBar()
-        self.status.showMessage("Ready")
-
-        # focus on the main widget
-        self.main_widget.setFocus()
-        self.setCentralWidget(self.main_widget)
-
-    # quit the gui
-    def fileQuit(self):
-        self.close()
-
-    # about box
-    def about(self):
-        QMessageBox.about(self, "About", \
-"""Skyflash tools
-
-A tool to configure and flash skybian default
-image to uSD cards for the official skyminer
-hardware of the Skycoin project.
-
-Skycoin: http://github.com/skycoin/skycoin
-Skybian: http://github.com/simelo/skybian
-Skyflash: http://github.com/simelo/skyflash
-
-""")
-
-app = QApplication(sys.argv)
-app.setOrganizationName("CO7WT Soft")
-app.setOrganizationDomain("co7wt.blogger.com")
-app.setApplicationName("Intrument PC interface")
-
-aw = ApplicationWindow()
-# aw.setWindowTitle("%s" % "Intrument PC interface")
-aw.show()
-sys.exit(app.exec_())
---
+++ skyflash.qml
@@ -0,0 +1,343 @@
+import QtQuick 2.9
+import QtQuick.Controls 1.4
+import QtQuick.Layouts 1.0
+import QtQuick.Dialogs 1.1
+
+ApplicationWindow {
+    id: windows
+    width: 398
+    //height: 420
+    height: 120
+    visible: true
+    title: qsTr("Skyflash tool")
+
+    // about dialog
+    MessageDialog {
+        id: aboutDialog
+        title: "About Skyflash"
+        text: "Skyflash is the official tool to create the\nSkyminers images from Skybian."
+        onAccepted: visible = false
+    }
+
+
+    FileDialog {
+        id: fileDialog
+        title: "Please choose a file"
+        folder: shortcuts.home
+        nameFilters: ["System image files (*.img)"]
+        selectMultiple: false
+
+        onAccepted: {
+            console.log("You chose to load: " + fileDialog.fileUrls)
+            skf.localFile(fileDialog.fileUrls)
+        }
+
+        onRejected: {
+            sbText.text = "You declined to choose a file."
+        }
+    }
+
+    menuBar: MenuBar {
+        Menu {
+            title: "&File"
+
+            MenuItem {
+                text: "E&xit"
+                shortcut: StandardKey.Quit
+                onTriggered: Qt.quit()
+            }
+        }
+
+        Menu {
+            title: "&Help"
+
+            MenuItem {
+                text: "Manual"
+                onTriggered: skf.openManual()
+            }
+
+            MenuItem {
+                text: "About..."
+                onTriggered: aboutDialog.open()
+            }
+        }
+    }
+
+    // main placeholder
+    ColumnLayout {
+        id: mainBox
+        anchors.left: parent.left
+        anchors.leftMargin: 3
+        anchors.top: parent.top
+        anchors.topMargin: 3
+        Layout.fillWidth: true
+        Layout.fillHeight: true
+        clip: false
+        visible: true
+
+        // image box
+        ColumnLayout {
+            id: boxImage
+            transformOrigin: Item.TopLeft
+            Layout.fillHeight: true
+            Layout.alignment: Qt.AlignLeft | Qt.AlignTop
+            spacing: 10
+            visible: true
+            Layout.fillWidth: true
+
+            // box title
+            Label {
+                text: "1. Get or select the Skybian base Image:"
+                font.pixelSize: 14
+                font.bold: true
+                color: "black"
+            }
+
+            // buttons
+            RowLayout {
+
+                // Download button
+                Button {
+                    id: btDown
+                    Layout.preferredHeight: 30
+                    Layout.preferredWidth: 80
+                    text: "Download"
+                    tooltip: "Click here to download the base Skybian image from the official site"
+
+                    onClicked: {
+                        // start download, hide the label and show progress bar
+                        lbImageComment.visible = false
+                        pbDownload.visible = true
+                        skf.downloadSkybian()
+                    }
+                }
+
+                // Browse button
+                Button {
+                    id: btBrowse
+                    Layout.preferredHeight: 30
+                    Layout.preferredWidth: 80
+                    text: "Browse"
+                    tooltip: "Click here to browse a already downloaded Skybian image"
+
+                    onClicked: {
+                        fileDialog.open()
+                    }
+                }
+
+                // label
+                Label {
+                    id: lbImageComment
+                    Layout.fillWidth: true
+                    text: "Path to local image"
+                    visible: false
+                }
+
+                // download ProgressBar
+                ProgressBar {
+                    id: pbDownload
+                    Layout.fillWidth: true
+                    visible: false
+                    maximumValue: 100
+                    minimumValue: 0
+                    value: 0.5
+                }
+            }
+        }
+
+        // network box
+        ColumnLayout {
+            id: boxNetwork
+            spacing: 10
+            visible: false
+
+            // box title
+            Label {
+                text: "2. Set network settings."
+                font.pixelSize: 14
+                font.bold: true
+                color: "black"
+            }
+
+            // defaults CheckBox
+            CheckBox {
+                text: "Use skyminner defaults"
+                checked: true
+
+                onCheckedChanged: {
+                    // TODO
+                }
+            }
+
+            // gateway
+            GridLayout {
+                columns: 2
+                rows: 4
+
+                // gateway
+                Label { text: "Network gateway:" }
+
+                TextField {
+                    id: txtGateway
+                    Layout.preferredWidth: 200
+                    placeholderText: "192.168.0.1"
+                    maximumLength: 16
+
+                }
+
+                // dns
+                Label { text: "Network DNS:" }
+
+                TextField  {
+                    id: txtDNS
+                    Layout.preferredWidth: 200
+                    placeholderText: "1.0.0.1, 1.1.1.1"
+                    maximumLength: 34
+                }
+
+                // manager IP
+                Label { text: "Manager IP:" }
+
+                TextField {
+                    id: txtManager
+                    Layout.preferredWidth: 200
+                    placeholderText: "192.168.0.2"
+                    maximumLength: 16
+
+                }
+
+                // node count
+                Label { text: "Node's count:" }
+
+                TextField  {
+                    id: txtNodes
+                    Layout.preferredWidth: 200
+                    placeholderText: "7"
+                    maximumLength: 5
+                }
+            }
+        }
+
+        // build box
+        ColumnLayout {
+            id: boxBuild
+            spacing: 10
+            visible: false
+
+            // box title
+            Label {
+                text: "3. Build the respective images."
+                font.pixelSize: 14
+                font.bold: true
+                color: "black"
+            }
+
+            // build tools
+            RowLayout {
+                // build button
+                Button {
+                    id: btBuild
+                    text: "Build the Images "
+
+                    onClicked: {
+                        // TODO
+                    }
+                }
+
+                ProgressBar {
+                    id: pbBuild
+                    Layout.fillWidth: true
+                    visible: true
+                    maximumValue: 100
+                    minimumValue: 0
+                    value: 7
+                }
+            }
+        }
+
+        // flash box
+        ColumnLayout {
+            id: boxFlash
+            spacing: 10
+            visible: false
+
+            // box title
+            Label {
+                text: "4. Flash the images."
+                font.pixelSize: 14
+                font.bold: true
+                color: "black"
+            }
+
+            // flash tools
+            RowLayout {
+                // pick your uSD
+                Label {
+                    id: lbSdCard
+                    text: "SD card:"
+                }
+
+                // ComboBox uSD
+                ComboBox {
+                    id: cbSdCard
+                    currentIndex: 0
+                    model: ListModel {
+                        id: cbSdCardItems
+                        ListElement { text: "Select..."; color: "Yellow" }
+                        ListElement { text: "/dev/mmcblk0"; color: "Green" }
+                        ListElement { text: "G:/"; color: "Brown" }
+                    }
+                    onCurrentIndexChanged: console.debug(cbSdCardItems.get(currentIndex).text + ", " + cbSdCardItems.get(currentIndex).color)
+                }
+
+                // flash ProgressBar
+                ProgressBar {
+                    id: pbFlash
+                    Layout.fillWidth: true
+                    visible: true
+                    maximumValue: 100
+                    minimumValue: 0
+                    value: 88
+                }
+            }
+        }
+    }
+
+    statusBar: StatusBar {
+        RowLayout {
+            anchors.fill: parent
+            Label { 
+                id: sbText
+                text: "Welcomed, please follow the steps"
+            }
+        }
+    }
+
+    // connection back to python
+    Connections {
+        target: skf
+
+        // receiving the percent of the download
+        onDownloadUpdate: {
+            pbDownload.value = percent
+        }
+
+        // status bar messages
+        onSetStatus: {
+            sbText.text = msg
+        }
+
+        // download / local done
+        onDownloadFinished: {
+            // inmediate actions
+            lbImageComment.text = msg
+            pbDownload.visible = false
+            lbImageComment.visible = true
+
+            // set next step visible
+            boxNetwork.visible = true
+            windows.height = 300
+
+        }
+    }
+}
