4e61e535e2323db6099a897e4acd9daf39ea2c9c
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-02-08 03:12:24 -0500

    Refs #1, Added and tested the integration of the generated .deb file with Ubuntu and Debian, WiP

--- makedeb.sh
+++ makedeb.sh
@@ -7,28 +7,37 @@
 
 
 # First step is to create the standalone executable
-# rm -rdf dist
-# rm -rdf build
-# rm -rdf __cache__
-# pyinstaller --add-data "skyflash.qml:skyflash.qml" \
-#     --clean \
-#     -F \
-#     skyflash.py
+rm -rdf dist
+rm -rdf build
+rm -rdf __cache__
+pyinstaller --add-data "skyflash.qml:skyflash.qml" \
+    --clean \
+    -F \
+    skyflash.py
 # exec is in ./dist/skyflash
 
+# some variables
+VER=0.0.3
+PKG=skyflash
+
 # Create the deb folder structure locally
-mkdir -p skyflash/usr/local/bin
-mkdir -p skyflash/DEBIAN
-cp dist/skyflash skyflash/usr/local/bin/
-cp skyflash.qml skyflash/usr/local/bin/
-cp skyflash-cli skyflash/usr/local/bin/
+mkdir -p $PKG/usr/local/bin
+mkdir -p $PKG/usr/share/applications/
+mkdir -p $PKG/usr/local/share/skybian/
+mkdir -p $PKG/DEBIAN
+cp dist/skyflash $PKG/usr/local/bin/
+cp skyflash.qml $PKG/usr/local/bin/
+cp skyflash-cli $PKG/usr/local/bin/
+cp deb/skyflash.desktop $PKG/usr/share/applications/
+cp deb/Skycoin.png $PKG/usr/local/share/skybian/
 
-size=`du -s skyflash/usr/local/bin | awk '{print $1}'`
+# dynamic values & other tricks
+size=`du -s $PKG/usr/local/bin | awk '{print $1}'`
 
-cat << EOF > skyflash/DEBIAN/control
-Package: skyflash
-Version: 0.0.3
-Maintainer: pavelmc@gmail.com
+cat << EOF > $PKG/DEBIAN/control
+Package: $PKG
+Version: $VER
+Maintainer: Pavel Milanes <pavelmc@gmail.com>
 Architecture: amd64
 Section: main
 Priority: optional
@@ -38,5 +47,5 @@ Description: A set of CLI & GUI tools to create your custom Skyminer OS images
 EOF
 
 # build the final deb package
-dpkg-deb --build --root-owner-group skyflash skyflash_0.0.3_amd64.deb
+dpkg-deb --build --root-owner-group $PKG "$PKG"_"$VER"_amd64.deb
 
