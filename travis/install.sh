f1c265605bfdaf0657e56cd91c395c4d77482f32
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-06-07 03:19:03 -0400

    Refs #21: fix pip upgrade syntax

--- travis/install.sh
+++ travis/install.sh
@@ -9,19 +9,19 @@ if [ $TRAVIS_OS_NAME = 'linux' ]; then
     # linux
     sudo apt-get update -q
     sudo apt install -y python3 python3-all python3-pip python3-pyqt5 python3-pyqt5.qtquick qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-extras qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings fakeroot python3-stdeb p7zip-full make
-    pip3 install --upgrade pip  # all three OSes agree about 'pip3'
+    pip install --upgrade pip  # all three OSes agree about 'pip3'
 fi
 
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     # macos
-    pip3 install --upgrade pip  # all three OSes agree about 'pip3'
+    pip install --upgrade pip  # all three OSes agree about 'pip3'
 fi
 
 
 if [ $TRAVIS_OS_NAME = 'windows' ]; then
     # windows
-    pip3 install --upgrade pip  # all three OSes agree about 'pip3'
+    pip install --upgrade pip  # all three OSes agree about 'pip3'
 fi
 
 # all 3 oses are agree about pip3, so direct here: install pyinstaller & pyqt5
