1a71a442527b6d695419532c2d6d8b26b70d9669
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-06-07 03:08:28 -0400

    Refs #21: fixing errors in travis draft, 1 of 3 (OSX Worked\!)

--- travis/install.sh
+++ travis/install.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # install step in travis, but split by OS
 
-
-# all 3 oses are agree about pip3, so direct here: install pyinstaller & pyqt5
-pip3 install setuptools pyqt5 PyInstaller
+# WARNING
+# 'python' points to Python 2.7 on macOS but points to Python 3.7 on Linux and Windows
+# 'python3' is a 'command not found' error on Windows but 'py' works on Windows only
 
 if [ $TRAVIS_OS_NAME = 'linux' ]; then
     # linux
     - sudo apt-get update -q
     - sudo apt install -y python3 python3-all python3-pip python3-pyqt5 python3-pyqt5.qtquick qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-extras qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings fakeroot python3-stdeb p7zip-full make
+    - pip3 install --upgrade pip  # all three OSes agree about 'pip3'
 fi
 
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     # macos
-    - echo "No particular depencency Yet"
+    - pip3 install --upgrade pip  # all three OSes agree about 'pip3'
 fi
 
 
 if [ $TRAVIS_OS_NAME = 'windows' ]; then
     # windows
-    # TODO 7z utility
-    - echo "No particular depencency Yet"
+    - pip3 install --upgrade pip  # all three OSes agree about 'pip3'
 fi
+
+# all 3 oses are agree about pip3, so direct here: install pyinstaller & pyqt5
+pip3 install setuptools pyqt5 PyInstaller
