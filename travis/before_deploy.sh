7f19a7135cc5fd914934e51ac8863886ee95b324
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-06-07 05:01:44 -0400

    Refs #21, fix before_deploy.sh syntax, bash not yaml

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -4,18 +4,18 @@
 
 if [ $TRAVIS_OS_NAME = 'linux' ]; then
     # linux
-    - make deb
-    - make linux-static
+    make deb
+    make linux-static
 fi
 
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     # macos
-    - make macos-app
+    make macos-app
 fi
 
 
 if [ $TRAVIS_OS_NAME = 'windows' ]; then
     # windows
-    - make win-travis
+    make win-travis
 fi
