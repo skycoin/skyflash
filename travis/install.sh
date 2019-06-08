ac842a20a02c9eb70fef2900ed99751bb4dbc545
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-06-08 00:56:01 -0400

    Refs #21, split dependencies by OS, ungrup linux & fake windows

--- travis/install.sh
+++ travis/install.sh
@@ -5,7 +5,7 @@
 # 'python' points to Python 2.7 on macOS but points to Python 3.7 on Linux and Windows
 # 'python3' is a 'command not found' error on Windows but 'py' works on Windows only
 
-if [ $1 = 'linux' || $1 = 'windows' ]; then
+if [ $1 = 'linux' ]; then
     # linux
     sudo apt-get update -q
     sudo apt install -y python3 python3-all python3-pip python3-pyqt5 python3-pyqt5.qtquick qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-extras qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings fakeroot python3-stdeb p7zip-full make
@@ -22,5 +22,9 @@ fi
 
 if [ $1 = 'windows' ]; then
     # windows
+    sudo apt-get update -q
+    sudo apt install -y python3 python3-all python3-pip python3-pyqt5 python3-pyqt5.qtquick qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-extras qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings fakeroot python3-stdeb p7zip-full make
+    pip3 install setuptools pyqt5
+    # pull the docker for windows
     docker pull cdrx/pyinstaller-windows
 fi
