ac34000a8ece282fcc728e7cad7ab5c3f9771f10
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-07-09 02:38:39 -0400

    Refs #65, simplify the travis dependency install script, making some requirement global

--- travis/install.sh
+++ travis/install.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 # install step in travis, but split by OS, os name is passed as first argument
 
-# WARNING
-# 'python' points to Python 2.7 on macOS but points to Python 3.7 on Linux and Windows
-# 'python3' is a 'command not found' error on Windows but 'py' works on Windows only
+# Common python requirements for all OSs
+pip3 install -U pip
+pip3 install -U pip setuptools pyqt5 PyInstaller requests pyqt5-sip sip 
 
 if [ $1 = 'linux' ]; then
     # linux
-    sudo apt-get update -q
-    sudo apt install -y make
-    # double pip install -U statement to get the second one with a latest version of the soft
-    pip3 install -U pip
-    pip3 install -U pip setuptools pyqt5 PyInstaller
+    # no particular action needed
 fi
 
 
 if [ $1 = 'osx' ]; then
     # macos
-    pip3 install -U pip setuptools pyqt5 PyInstaller
+    # no particular action needed
 fi
 
 
 if [ $1 = 'windows' ]; then
     # windows
-    sudo apt-get update -q
-    sudo apt install -y p7zip-full make
-    # double pip install -U statement to get the second one with a latest version of the soft
-    pip3 install -U pip
-    pip3 install -U setuptools pyqt5
+
+    # compression tool for the portable app
+    sudo apt install -y p7zip-full
 
     # pull the docker for windows
     docker pull cdrx/pyinstaller-windows
